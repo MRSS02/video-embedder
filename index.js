6604e93ff2e61670a7e1491decf725c17a9fc341
MRSS <mateusyasd@hotmail.com>
2023-12-27 18:02:03 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 80;
+const app = express();
+
+app.get('/*', (req, res) => {
+  
+  const html = `<!DOCTYPE html>
+    <html lang="en">
+      <head>
+        
+        /<meta property="og:url" content="${req.headers.host}${req.url}" />
+        <meta property="og:video" content="https://catbox.moe/${req.url}">
+        <meta property="og:video:url" content="https://catbox.moe/${req.url}">
+        <meta property="og:video:secure:url" content="https://catbox.moe/${req.url}"> 
+        <meta property="og:video:type" content="video/mp4">
+        <meta property="og:video:width" content="1280">
+        <meta property="og:video:height" content="720">
+
+      </head>
+      <body>
+        <div id="root">
+            <h1><a ref="https://files.catbox.moe/${req.url}">https://files.catbox.moe/${req.url}</h1>
+           <iframe src="https://files.catbox.moe/${req.url}"></iframe> 
+        </div>
+      </body>
+    </html>
+  `
+
+  res.send(html);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
