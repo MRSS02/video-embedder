e907fae49718dde405ddfeff278687621533b8a2
MRSS <mateusyasd@hotmail.com>
2023-12-27 18:27:18 -0300

    changes

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ app.get('/*', (req, res) => {
       <head>
         <meta name="twitter:card" content="player">
         <meta name="twitter:site" content="@streamable">
-        <meta name="twitter:image" content="https://catbox.moe/${req.url}">
+        <meta name="twitter:image" content="https://files.catbox.moe/${req.url}">
         <meta name="twitter:player:width" content="1280">
         <meta name="twitter:player:height" content="720">
-        <meta name="twitter:player" content="https://catbox.moe/${req.url}">
-        <link rel="amphtml" href="https://catbox.moe/${req.url}">
+        <meta name="twitter:player" content="https://files.catbox.moe/${req.url}">
+        <link rel="amphtml" href="https://files.catbox.moe/${req.url}">
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:image:width" content="1280">
         <meta property="og:image:height" content="720">
         <meta property="og:url" content="${req.headers.host}${req.url}" />
-        <meta property="og:video" content="https://catbox.moe/${req.url}">
-        <meta property="og:video:url" content="https://catbox.moe/${req.url}">
-        <meta property="og:video:secure:url" content="https://catbox.moe/${req.url}"> 
+        <meta property="og:video" content="https://files.catbox.moe/${req.url}">
+        <meta property="og:video:url" content="https://files.catbox.moe/${req.url}">
+        <meta property="og:video:secure:url" content="https://files.catbox.moe/${req.url}"> 
         <meta property="og:video:type" content="video/mp4">
         <meta property="og:video:width" content="1280">
         <meta property="og:video:height" content="720">
