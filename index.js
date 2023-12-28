cd758e9ca44d5c85fff8f4cc6bd8db7ea04cf874
MRSS <mateusyasd@hotmail.com>
2023-12-28 16:36:29 -0300

    fixes to text in homepage

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.get('/*', (req, res) => {
         <h2>MParaGames's Catbox Video Embedder</h2>
 
         <p>This is a simple video embedder for Discord that embbeds a video uploaded to <a href="https://catbox.moe">catbox.moe</a>. 
-        Only supports video uploads from catbox. Only mp4 support was tested, h264, VP8, VP9, and AV1 videos should work.
+        Only supports video uploads from catbox. Only mp4 support was tested (support for webm will be added), h264, VP8, VP9, and AV1 videos should work.
         <br/><br/>Usage is simple, just replace <a href="https://catbox.moe">catbox.moe</a> with <a href="https://video.starmain.dev">video.starmain.dev</a>
         in your uploaded video link.<br/>If you want your video to have a custom name, just put a query on the paramenter name on it.
         Example: <a href="https://files.catbox.moe/w1tr9y.mp4">https://files.catbox.moe/w1tr9y.mp4</a> can become <a href="https://video.starmain.dev/w1tr9y.mp4?name=my%20Forza%20Horizon%20video">
