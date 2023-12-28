91bb2b2c13ee4cb58527245877124543c69fd393
MRSS <mateusyasd@hotmail.com>
2023-12-27 23:38:19 -0300

    additional changes to metadata

--- index.js
+++ index.js
@@ -9,40 +9,42 @@ app.get('/*', (req, res) => {
     <html lang="en">
       <head>
         <style>
-          root {
+          #root {
             color: #333333;
           }
         </style>
         <meta name="theme-color" content="#00FFAA" />
-        <meta name="description" content="Video Preview">
+        <meta name="description" content="Video Embedder">
         <meta name="twitter:card" content="player">
         <meta name="twitter:site" content="Video">
         <meta name="twitter:image" content="https://files.catbox.moe/rh0eua.webp">
         <meta name="twitter:player:width" content="1280">
         <meta name="twitter:player:height" content="720">
-        <meta name="twitter:player:stream" content="https://files.catbox.moe${req.url}">
-        <meta name="twitter:player" content="https://files.catbox.moe${req.url}">
-        <link rel="amphtml" href="https://files.catbox.moe${req.url}">
+        <meta name="twitter:player:stream" content="https://files.catbox.moe${req.path}">
+        <meta name="twitter:player" content="https://files.catbox.moe${req.path}">
+        <link rel="amphtml" href="https://files.catbox.moe${req.path}">
         <meta property="og:type" content="video.other">
         <meta property="og:image:type" content="image/webp" />
         <meta property="og:image:width" content="1280">
         <meta property="og:image:height" content="720">
         <meta property="og:image" content="https://files.catbox.moe/rh0eua.webp">
-        <meta property="og:url" content="${req.headers.host}${req.url}" />
-        <meta property="og:video" content="https://files.catbox.moe/${req.url}">
-        <meta property="og:video:url" content="https://files.catbox.moe/${req.url}">
-        <meta property="og:video:secure:url" content="https://files.catbox.moe/${req.url}"> 
+        <meta property="og:url" content="${req.headers.host}${req.path}" />
+        <meta property="og:video" content="https://files.catbox.moe/${req.path}">
+        <meta property="og:video:url" content="https://files.catbox.moe/${req.path}">
+        <meta property="og:video:secure:url" content="https://files.catbox.moe/${req.path}"> 
         <meta property="og:video:type" content="video/mp4">
         <meta property="og:video:width" content="1280">
         <meta property="og:video:height" content="720">
-        <meta content="Video Embedder" property="og:title" />
-
+        <meta content="${req.query.name || "Video Embedder"}" property="og:title" />
+        
       </head>
       <body>
         <div id="root">
-            <h1><a href="https://files.catbox.moe${req.url}">https://files.catbox.moe${req.url}</a></h1>
+            
+            <h1>MParaGame's video embedder${req.quety.name ? ' - ' + req.query.name || ""}</h1>
+            <h3><a href="https://files.catbox.moe${req.path}">https://files.catbox.moe${req.path}</a></h3>
             <div>
-              <video width='1280' height='720' controls preload='metadata'><source src="https://files.catbox.moe${req.url}" type='video/mp4'></video>
+              <video width='1280' height='720' controls preload='metadata'><source src="https://files.catbox.moe${req.path}" type='video/mp4'></video>
             </div>
       </body>
     </html>
