2487ec828948bd87171653cbd0e5bf3dd3283d21
MRSS <mateusyasd@hotmail.com>
2023-12-28 16:30:28 -0300

    fixes to text in homepage

--- index.js
+++ index.js
@@ -27,6 +27,9 @@ app.get('/*', (req, res) => {
             text-decoration: none;
             text-align: center;
           }
+          smalltext {
+            text-align: center;
+          }
       </style>
       <meta charset="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -40,8 +43,8 @@ app.get('/*', (req, res) => {
         <p>This is a simple video embedder for Discord that embbeds a video uploaded to <a href="https://catbox.moe">catbox.moe</a>. 
         Only supports video uploads from catbox. Only mp4 support was tested, h264, VP8, VP9, and AV1 videos should work.
         <br/><br/>Usage is simple, just replace <a href="https://catbox.moe">catbox.moe</a> with <a href="https://video.starmain.dev">video.starmain.dev</a>
-        in your uploaded video link. If you want your video to have a custom name, just put a query on the paramenter name on it.
-        <br/>Example: <a href="https://files.catbox.moe/w1tr9y.mp4">https://files.catbox.moe/w1tr9y.mp4</a> can become <a href="https://video.starmain.dev/w1tr9y.mp4?name=my%20Forza%20Horizon%20video">
+        in your uploaded video link.<br/>If you want your video to have a custom name, just put a query on the paramenter name on it.
+        Example: <a href="https://files.catbox.moe/w1tr9y.mp4">https://files.catbox.moe/w1tr9y.mp4</a> can become <a href="https://video.starmain.dev/w1tr9y.mp4?name=my%20Forza%20Horizon%20video">
         https://video.starmain.dev/w1tr9y.mp4?name=my%20Forza%20Horizon%20video</a> for a video named "My Forza Horizon Video".</p>
         <p class="smalltext">Check the source code in my <a href="https://github.com/MRSS02/video-embedder">Github</a>!</p>
      </body>
