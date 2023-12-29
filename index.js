d4021d37ec436ce2365446eaec317e74d2ff44a3
MRSS <mateusyasd@hotmail.com>
2023-12-28 22:12:47 -0300

    testing what happens on no image

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.get('/*', (req, res) => {
         <meta property="og:image:width" content="1280">
         <meta property="og:image:height" content="720">
         <meta property="og:site_name" content="MParaGames' catbox video embedder"/>
-        ${req.query?.av1 ? <meta property="og:image" content="https://files.catbox.moe/rh0eua.webp"> : ""}
+        ${req.query?.av1 ? "<meta property='og:image' content='https://files.catbox.moe/rh0eua.webp'>" : ""}
         <meta property="og:url" content="${req.headers.host}${req.path}" />
         <meta property="og:video" content="https://files.catbox.moe/${req.path}">
         <meta property="og:video:url" content="https://files.catbox.moe/${req.path}">
