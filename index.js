3755a352ae814a8e47251d15cc7214db5288e9ab
MRSS <mateusyasd@hotmail.com>
2023-12-28 22:48:41 -0300

    reversing no image

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.get('/*', (req, res) => {
         <meta name="description" content="Embbed catbox videos on discord!">
         <meta name="twitter:card" content="player">
         <meta name="twitter:site" content="Video">
-        ${req.query?.av1 ? "<meta name='twitter:image' content='https://files.catbox.moe/rh0eua.webp' />"  : ""}
+        <meta name='twitter:image' content='https://files.catbox.moe/rh0eua.webp' />
         <meta name="twitter:player:width" content="1280">
         <meta name="twitter:player:height" content="720">
         <meta name="twitter:player:stream" content="https://files.catbox.moe${req.path}">
@@ -110,7 +110,7 @@ app.get('/*', (req, res) => {
         <meta property="og:image:width" content="1280">
         <meta property="og:image:height" content="720">
         <meta property="og:site_name" content="MParaGames' catbox video embedder"/>
-        ${req.query?.av1 ? "<meta property='og:image' content='https://files.catbox.moe/rh0eua.webp'>" : ""}
+        <meta property='og:image' content='https://files.catbox.moe/rh0eua.webp'>
         <meta property="og:url" content="${req.headers.host}${req.path}" />
         <meta property="og:video" content="https://files.catbox.moe/${req.path}">
         <meta property="og:video:url" content="https://files.catbox.moe/${req.path}">
