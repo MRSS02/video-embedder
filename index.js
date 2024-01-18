436c9208af409a205bc840b9b4eb31040a94c80e
MRSS <mateusyasd@hotmail.com>
2024-01-18 10:59:27 -0300

    test with image

--- index.js
+++ index.js
@@ -100,18 +100,18 @@ app.get('/*', (req, res) => {
         <meta name="description" content="Embbed catbox videos on discord!">
         <meta name="twitter:card" content="player">
         <meta name="twitter:site" content="Video">
-        <!--meta name="twitter:image" content='https://files.catbox.moe/rh0eua.webp' -->
+        <meta name="twitter:image" content='https://files.catbox.moe/rh0eua.webp'>
         <meta name="twitter:player:width" content="1280">
         <meta name="twitter:player:height" content="720">
         <meta name="twitter:player:stream" content="https://files.catbox.moe${req.path}">
         <meta name="twitter:player" content="https://files.catbox.moe${req.path}">
         <link rel="amphtml" href="https://files.catbox.moe${req.path}">
         <meta property="og:type" content="video.other">
-        <!--meta property="og:image:type" content="image/webp"-->
-        <!--meta property="og:image:width" content="1280" -->
-        <!--meta property="og:image:height" content="720" -->
+        <meta property="og:image:type" content="image/webp">
+        <meta property="og:image:width" content="1280">
+        <meta property="og:image:height" content="720">
         <meta property="og:site_name" content="MParaGames' Catbox video embedder"/>
-        <!--meta property="og:image" content="https://files.catbox.moe/rh0eua.webp"-->
+        <meta property="og:image" content="https://files.catbox.moe/rh0eua.webp">
         <meta property="og:url" content="${req.headers.host}${req.path}" />
         <meta property="og:video" content="https://files.catbox.moe${req.path}">
         <meta property="og:video:url" content="https://files.catbox.moe${req.path}">
