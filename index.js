91589474a17c736e2281f44e279d475cfbf18375
MRSS <mateusyasd@hotmail.com>
2024-01-18 10:45:42 -0300

    fixed url link

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ app.get('/*', (req, res) => {
         <!--meta property="og:image:width" content="1280"-->
         <!--meta property="og:image:height" content="720"-->
         <meta property="og:site_name" content="MParaGames' Catbox video embedder"/>
-        <meta property="og:image" content=https://files.catbox.moe/rh0eua.webp">
+        <meta property="og:image" content="https://files.catbox.moe/rh0eua.webp">
         <meta property="og:url" content="${req.headers.host}${req.path}" />
         <meta property="og:video" content="https://files.catbox.moe${req.path}">
         <meta property="og:video:url" content="https://files.catbox.moe${req.path}">
