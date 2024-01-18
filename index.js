041c286a8029b2b6be7829909274ad0de04d2332
MRSS <mateusyasd@hotmail.com>
2024-01-18 10:43:19 -0300

    fixed url link

--- index.js
+++ index.js
@@ -111,11 +111,11 @@ app.get('/*', (req, res) => {
         <!--meta property="og:image:width" content="1280"-->
         <!--meta property="og:image:height" content="720"-->
         <meta property="og:site_name" content="MParaGames' Catbox video embedder"/>
-        <meta property="og:image" content='https://files.catbox.moe/rh0eua.webp'>
+        <meta property="og:image" content=https://files.catbox.moe/rh0eua.webp">
         <meta property="og:url" content="${req.headers.host}${req.path}" />
-        <meta property="og:video" content="https://files.catbox.moe/${req.path}">
-        <meta property="og:video:url" content="https://files.catbox.moe/${req.path}">
-        <meta property="og:video:secure:url" content="https://files.catbox.moe/${req.path}"> 
+        <meta property="og:video" content="https://files.catbox.moe${req.path}">
+        <meta property="og:video:url" content="https://files.catbox.moe${req.path}">
+        <meta property="og:video:secure:url" content="https://files.catbox.moe${req.path}"> 
         <meta name="twitter:creator" content="MParaGames' Catbox Video Embedder">
         ${req.path.slice(req.path.lastIndexOf(".") + 1) === "mp4" ? "<meta property='og:video:type' content='video/mp4'>" : ""}
         ${req.path.slice(req.path.lastIndexOf(".") + 1) === "webm" ? "<meta property='og:video:type' content='video/webm'>" : ""}
